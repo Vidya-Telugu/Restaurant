88598dce53d7f0e4709fb14792627e36cb41757b
Vidya-Telugu <vidyatelugu2023@gmail.com>
2024-10-07 07:28:06 +0530

    ic

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import HeaderCartButton from "./HeaderCartButton";
-import imageforheader from "../assets/imageforheader.jpg";
+//import imageforheader from "../assets/imageforheader.jpg";
 import "./Header.css";
 function Header() {
   return (
     <div className="container">
-      <header>
-        <h3>ReactMeals</h3>
+      <header className="header">
+        <h3>React Restaurant</h3>
         <HeaderCartButton></HeaderCartButton>
       </header>
-      <div className="img-div">
-        <img src={imageforheader}></img>
+      <div className='card-desc'>
+          <h3 className="desc-heading">Delicious Food,Delivered To You</h3>
+          <p className='desc-content'>Choose your favourite meal from broad selection of available meals and
+            enjoy a delicious lunch or meal at home.</p>
+            <p>All our meals are cooked with high quality ingredients just-in-time 
+              and ofcourse by experienced chefs</p>
       </div>
     </div>
   )
--- src/Layout/HeaderCartButton.jsx
+++ src/Layout/HeaderCartButton.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "./HeaderCartButton.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon component
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';  
 //import cartfavicon from "../assets/carticon.png";
 function HeaderCartButton() {
   return (
-   <button>
-    <span className='img-icon'>icon</span>
-    <span>Your Cart</span>
-    <span>0</span>
+   <button className='cart-btn'>
+    <span className='img-icon'><FontAwesomeIcon icon={faCartPlus} /></span>
+    <span className='Cart-name'>Your Cart </span>
+    <span className='items-count'>0</span>
    </button>
   )
 }
